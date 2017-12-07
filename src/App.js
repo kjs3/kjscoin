b601fc2fd2872982d7d009ef0d92010d376d3197
Kenny Smith <mail@kjs3.com>
2017-12-07 12:56:51 -0800

    init commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  render () {
+    return (
+      <main>
+        <header>
+          <h1>kjscoin</h1>
+          <h2>A Blockchain Demo</h2>
+        </header>
+      </main>
+    )
+  }
+}
+
+export default App
